f280f15433475a9bbf9c49f345812cf7417a25ee
Mikail <2004.01041@manas.edu.kg>
2025-04-17 14:44:28 +0600

    wishlist prepairing

--- src/app/(root)/profile/layout.jsx
+++ src/app/(root)/profile/layout.jsx
@@ -37,8 +37,8 @@ export default function ProfileLayout({ children }) {
   if (loading || !checkedAuth) return null // или <Spinner /> если хочешь
 
   return (
-    <main>
-      <div className="w-full py-4 bg-gray-100 my-5">
+    <div className={'bg-gray-200 h-full'}>
+      <div className="w-full py-4 my-5">
         <h1 className="max-w-screen-2xl mx-auto text-xl font-semibold px-2">
           Мой профиль
         </h1>
@@ -49,6 +49,6 @@ export default function ProfileLayout({ children }) {
         </div>
         <div className="w-3/4">{children}</div>
       </div>
-    </main>
+    </div>
   )
 }
--- src/components/root/profile/UserInfo.jsx
+++ src/components/root/profile/UserInfo.jsx
@@ -5,12 +5,21 @@ import { useEffect, useRef, useState } from 'react'
 import { ChevronDownIcon } from 'lucide-react'
 import useUserStore from '@/service/stores/useUser.store'
 
-const genderData = ['Мужской', 'Женский', 'Не выбрано']
+const genderData = [
+  { title: 'мужской', value: 'male' },
+  { title: 'женский', value: 'female' },
+  { title: 'не выбрано', value: null },
+]
 
 export const UserInfo = () => {
   const [userInfo, setUserInfo] = useState({ name: '', email: '' })
   const [genderSelectOpen, setGenderSelectOpen] = useState(false)
-  const [selectedGender, setSelectedGender] = useState(genderData[2])
+  const [selectedGenderValue, setSelectedGenderValue] = useState(
+    genderData[2].value
+  )
+  const [selectedGenderTitle, setSelectedGenderTitle] = useState(
+    genderData[2].title
+  )
   const [avatarFile, setAvatarFile] = useState(null)
   const [avatarPreview, setAvatarPreview] = useState('/placeholder/male.png')
   const [isPasswordChangeOpen, setIsPasswordChangeOpen] = useState(false)
@@ -31,7 +40,7 @@ export const UserInfo = () => {
 
   const UpdateProfileData = async () => {
     const formData = new FormData()
-    formData.append('gender', selectedGender)
+    formData.append('gender', selectedGenderValue)
     formData.append('name', userInfo.name || profile?.name || '')
     formData.append('email', userInfo.email || profile?.email || '')
     if (avatarFile) {
@@ -49,11 +58,13 @@ export const UserInfo = () => {
     }
 
     if (profile) {
+      console.log(profile)
       setUserInfo({ name: profile.name || '', email: profile.email || '' })
-      if (profile.avatarUrl) {
-        setAvatarPreview(profile.avatarUrl)
+      if (profile.avatar) {
+        setAvatarPreview(profile.avatar)
       }
-      setSelectedGender(profile.gender || genderData[2])
+      setSelectedGenderValue(profile.gender || genderData[2].value)
+      setSelectedGenderTitle(profile.gender === 'male' ? 'мужской' : 'женский')
     }
 
     document.addEventListener('mousedown', handleClickOutside)
@@ -62,71 +73,78 @@ export const UserInfo = () => {
   }, [profile])
 
   return (
-    <div className="bg-gray-100 rounded-md p-10">
-      <h1 className="text-3xl font-bold">Мой профиль</h1>
-      <div className="bg-gray-500 h-[1px] w-full my-5" />
-      <div className="bg-white rounded-md p-3 flex justify-between items-start">
-        <div className="flex justify-start items-center gap-4 w-3/4">
-          <div
-            className="border p-1 rounded-sm w-52 aspect-square cursor-pointer"
-            onClick={() => fileInputRef.current?.click()}
+    <div className="rounded-md">
+      <div className="bg-white rounded-md p-10">
+        <h1 className="text-3xl font-bold">Мой профиль</h1>
+        <div className="bg-gray-500 h-[1px] w-full my-5" />
+        <div className="flex justify-between items-start">
+          <div className="flex justify-start items-center gap-4 w-3/4">
+            <div
+              className="border p-1 rounded-sm w-52 aspect-square cursor-pointer"
+              onClick={() => fileInputRef.current?.click()}
+            >
+              <Image
+                src={avatarPreview}
+                alt="avatar"
+                width={200}
+                height={200}
+                unoptimized
+                className="rounded-sm object-cover w-full h-full"
+              />
+              <input
+                type="file"
+                accept="image/*"
+                ref={fileInputRef}
+                onChange={handleAvatarChange}
+                className="hidden"
+              />
+            </div>
+            <div className="space-y-5 flex-1">
+              <div className="flex flex-col gap-2">
+                <label className="text-xl text-gray-600 font-normal">Имя</label>
+                <input
+                  value={userInfo.name}
+                  onChange={(e) =>
+                    setUserInfo((prev) => ({ ...prev, name: e.target.value }))
+                  }
+                  className="text-xl font-semibold border rounded-md py-1 px-3 border-slate-500 w-full"
+                  type="text"
+                  placeholder="ФИО"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="text-xl text-gray-600 font-normal">
+                  Электронный адрес
+                </label>
+                <input
+                  value={userInfo.email}
+                  onChange={(e) =>
+                    setUserInfo((prev) => ({ ...prev, email: e.target.value }))
+                  }
+                  className="text-xl font-semibold border rounded-md py-1 px-3 border-slate-500 w-full"
+                  type="text"
+                  placeholder="Электронный адрес"
+                />
+              </div>
+            </div>
+          </div>
+          <button
+            onClick={UpdateProfileData}
+            className="text-blue-500 bg-blue-500/30 hover:bg-blue-500/40 cursor-pointer duration-200 py-2 px-5 rounded-md text-xl"
           >
-            <Image
-              src={avatarPreview}
-              alt="avatar"
-              width={200}
-              height={200}
-              unoptimized
-              className="rounded-sm object-cover w-full h-full"
-            />
-            <input
-              type="file"
-              accept="image/*"
-              ref={fileInputRef}
-              onChange={handleAvatarChange}
-              className="hidden"
-            />
-          </div>
-          <div className="space-y-5 flex-1">
-            <div className="flex flex-col gap-2">
-              <label className="text-xl text-gray-600 font-normal">Имя</label>
-              <input
-                value={userInfo.name}
-                onChange={(e) =>
-                  setUserInfo((prev) => ({ ...prev, name: e.target.value }))
-                }
-                className="text-xl font-semibold border rounded-md py-1 px-3 border-slate-500 w-full"
-                type="text"
-                placeholder="ФИО"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="text-xl text-gray-600 font-normal">Электронный адрес</label>
-              <input
-                value={userInfo.email}
-                onChange={(e) =>
-                  setUserInfo((prev) => ({ ...prev, email: e.target.value }))
-                }
-                className="text-xl font-semibold border rounded-md py-1 px-3 border-slate-500 w-full"
-                type="text"
-                placeholder="Электронный адрес"
-              />
-            </div>
-          </div>
+            Редактировать
+          </button>
         </div>
-        <button
-          onClick={UpdateProfileData}
-          className="text-blue-500 bg-blue-500/30 hover:bg-blue-500/40 cursor-pointer duration-200 py-2 px-5 rounded-md text-xl"
-        >
-          Редактировать
-        </button>
       </div>
 
-      <div className="bg-white rounded-md p-3 mt-4">
+      <div className="bg-white rounded-md p-10 mt-4  ">
         <h1 className="text-xl font-bold mb-4">Данные профиля</h1>
+        <div className="bg-gray-500 h-[1px] w-full my-5" />
         <div className="flex items-center justify-between gap-4">
           <div className="w-1/2">
-            <label className="text-xl text-gray-600 font-normal">Номер телефона</label>
+            <label className="text-xl text-gray-600 font-normal">
+              Номер телефона
+            </label>
             <input
               type="text"
               className="mt-2 text-xl font-semibold border rounded-md py-2 px-3 border-slate-500 w-full"
@@ -139,7 +157,7 @@ export const UserInfo = () => {
               className="mt-2 flex items-center justify-between border border-slate-500 rounded-md py-2 px-3 cursor-pointer select-none text-xl font-semibold"
               onClick={() => setGenderSelectOpen((prev) => !prev)}
             >
-              <span>{selectedGender}</span>
+              <span>{selectedGenderTitle}</span>
               <ChevronDownIcon
                 className={`text-gray-400 transition-transform duration-300 ${
                   genderSelectOpen ? 'rotate-180' : 'rotate-0'
@@ -149,19 +167,22 @@ export const UserInfo = () => {
 
             <div
               className={`absolute z-10 left-0 right-0 mt-1 bg-white border border-slate-300 rounded-md shadow-md overflow-hidden transition-all duration-200 origin-top ${
-                genderSelectOpen ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-0 pointer-events-none'
+                genderSelectOpen
+                  ? 'scale-y-100 opacity-100'
+                  : 'scale-y-0 opacity-0 pointer-events-none'
               }`}
             >
               {genderData.map((item, index) => (
                 <div
                   key={index}
                   onClick={() => {
-                    setSelectedGender(item==='мужской'? 'male' : 'female')
+                    setSelectedGenderValue(item.value)
+                    setSelectedGenderTitle(item.title)
                     setGenderSelectOpen(false)
                   }}
                   className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
                 >
-                  {item === 'male' ? 'мужской' : 'женский'}
+                  {item.title}
                 </div>
               ))}
             </div>
@@ -169,19 +190,24 @@ export const UserInfo = () => {
         </div>
       </div>
 
-      <div className="bg-white rounded-md p-3 mt-4">
-        <div className="flex justify-between items-center gap-4">
-          <h1 className="text-xl font-bold">Данные входа</h1>
-          <button
-            className="bg-blue-500/20 text-blue-500 hover:bg-blue-500/30 py-2 px-5 rounded-md text-xl"
-            onClick={() => setIsPasswordChangeOpen((prev) => !prev)}
-          >
-            {isPasswordChangeOpen ? 'Сохранить' : 'Изменить пароль'}
-          </button>
+      <div className="bg-white rounded-md p-10 my-4">
+        <div className="">
+          <div className="flex justify-between items-center gap-4">
+            <h1 className="text-xl font-bold">Данные входа</h1>
+            <button
+              className="bg-blue-500/20 text-blue-500 hover:bg-blue-500/30 py-2 px-5 rounded-md text-xl"
+              onClick={() => setIsPasswordChangeOpen((prev) => !prev)}
+            >
+              {isPasswordChangeOpen ? 'Сохранить' : 'Изменить пароль'}
+            </button>
+          </div>
+          <div className="bg-gray-500 h-[1px] w-full my-5" />
         </div>
         <div
           className={`transition-all duration-500 overflow-hidden ${
-            isPasswordChangeOpen ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'
+            isPasswordChangeOpen
+              ? 'max-h-[500px] opacity-100'
+              : 'max-h-0 opacity-0'
           }`}
         >
           <div className="flex justify-between items-center gap-4 mt-4">
