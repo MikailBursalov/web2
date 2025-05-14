67e85c87b1733ed9f3760b8e68cb341d60b0ee85
Mikail Bursalov <2004.01041@manas.edu.kg>
2025-05-14 17:30:15 +0600

    final commit

--- src/components/root/CatalogContent.jsx
+++ src/components/root/CatalogContent.jsx
@@ -5,203 +5,40 @@ import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import usePropertyStore from '@/service/stores/useProperty.store'
 
-const catalogData = [
-  {
-    image: '/suggestedListings/image1.jpg',
-    isInFavorite: false,
-    price: 50000,
-    paymentPeriod: 'month',
-    roomCount: 4,
-    area: 134,
-    floor: 3,
-    totalFloors: 9,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image2.jpg',
-    isInFavorite: false,
-    price: 890000,
-    paymentPeriod: 'year',
-    roomCount: 3,
-    area: 105,
-    floor: 2,
-    totalFloors: 12,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image3.jpg',
-    isInFavorite: true,
-    price: 12000,
-    paymentPeriod: 'month',
-    roomCount: 1,
-    area: 52,
-    floor: 8,
-    totalFloors: 14,
-    address: 'Микрорайон Джал-23,Ленинский район, Бишкек, 720044, 5 этажей',
-  },
-  {
-    image: '/suggestedListings/image4.jpg',
-    isInFavorite: false,
-    price: 30000,
-    paymentPeriod: 'month',
-    roomCount: 2,
-    area: 78,
-    floor: 5,
-    totalFloors: 20,
-    address: 'Улица Токтогула, 13, Первомайский район, Бишкек, 720001, 1 этаж',
-  },
-  {
-    image: '/suggestedListings/image1.jpg',
-    isInFavorite: false,
-    price: 50000,
-    paymentPeriod: 'month',
-    roomCount: 4,
-    area: 134,
-    floor: 3,
-    totalFloors: 9,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image2.jpg',
-    isInFavorite: false,
-    price: 890000,
-    paymentPeriod: 'year',
-    roomCount: 3,
-    area: 105,
-    floor: 2,
-    totalFloors: 12,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image3.jpg',
-    isInFavorite: true,
-    price: 12000,
-    paymentPeriod: 'month',
-    roomCount: 1,
-    area: 52,
-    floor: 8,
-    totalFloors: 14,
-    address: 'Микрорайон Джал-23,Ленинский район, Бишкек, 720044, 5 этажей',
-  },
-  {
-    image: '/suggestedListings/image4.jpg',
-    isInFavorite: false,
-    price: 30000,
-    paymentPeriod: 'month',
-    roomCount: 2,
-    area: 78,
-    floor: 5,
-    totalFloors: 20,
-    address: 'Улица Токтогула, 13, Первомайский район, Бишкек, 720001, 1 этаж',
-  },
-  {
-    image: '/suggestedListings/image1.jpg',
-    isInFavorite: false,
-    price: 50000,
-    paymentPeriod: 'month',
-    roomCount: 4,
-    area: 134,
-    floor: 3,
-    totalFloors: 9,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image2.jpg',
-    isInFavorite: false,
-    price: 890000,
-    paymentPeriod: 'year',
-    roomCount: 3,
-    area: 105,
-    floor: 2,
-    totalFloors: 12,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image3.jpg',
-    isInFavorite: true,
-    price: 12000,
-    paymentPeriod: 'month',
-    roomCount: 1,
-    area: 52,
-    floor: 8,
-    totalFloors: 14,
-    address: 'Микрорайон Джал-23,Ленинский район, Бишкек, 720044, 5 этажей',
-  },
-  {
-    image: '/suggestedListings/image4.jpg',
-    isInFavorite: false,
-    price: 30000,
-    paymentPeriod: 'month',
-    roomCount: 2,
-    area: 78,
-    floor: 5,
-    totalFloors: 20,
-    address: 'Улица Токтогула, 13, Первомайский район, Бишкек, 720001, 1 этаж',
-  },
-  {
-    image: '/suggestedListings/image1.jpg',
-    isInFavorite: false,
-    price: 50000,
-    paymentPeriod: 'month',
-    roomCount: 4,
-    area: 134,
-    floor: 3,
-    totalFloors: 9,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image2.jpg',
-    isInFavorite: false,
-    price: 890000,
-    paymentPeriod: 'year',
-    roomCount: 3,
-    area: 105,
-    floor: 2,
-    totalFloors: 12,
-    address: '8-й м-н, дом 17, Октябрьский район, Бишкек, 720075',
-  },
-  {
-    image: '/suggestedListings/image3.jpg',
-    isInFavorite: true,
-    price: 12000,
-    paymentPeriod: 'month',
-    roomCount: 1,
-    area: 52,
-    floor: 8,
-    totalFloors: 14,
-    address: 'Микрорайон Джал-23,Ленинский район, Бишкек, 720044, 5 этажей',
-  },
-  {
-    image: '/suggestedListings/image4.jpg',
-    isInFavorite: false,
-    price: 30000,
-    paymentPeriod: 'month',
-    roomCount: 2,
-    area: 78,
-    floor: 5,
-    totalFloors: 20,
-    address: 'Улица Токтогула, 13, Первомайский район, Бишкек, 720001, 1 этаж',
-  },
-]
+const formatPriceLabel = (price) => {
+  if (!price?.amount || !price?.currency || !price?.paymentPeriod)
+    return 'цена не указана'
+
+  const currencySymbol = price.currency === 'USD' ? '$' : 'C'
+  const periodLabel =
+    {
+      daily: '/день',
+      weekly: '/неделя',
+      monthly: '/мес',
+      yearly: '/год',
+    }[price.paymentPeriod] || ''
+
+  return `${price.amount} ${currencySymbol}${periodLabel}`
+}
 
 export const CatalogContent = () => {
   const { push } = useRouter()
   const { properties, fetchProperties } = usePropertyStore()
+
   const handleClickProduct = (item) => {
     localStorage.setItem('selectedProperty', JSON.stringify(item))
     push(`/catalog/${item._id}`)
   }
-  console.log(properties)
+
   useEffect(() => {
     fetchProperties()
   }, [])
+
   return (
-    <div className={`bg-white px-8 py-4 rounded-md`}>
-      <div className={`max-w-screen-2xl mx-auto`}>
+    <div className="bg-white px-8 py-4 rounded-md">
+      <div className="max-w-screen-2xl mx-auto">
         <div>
-          <h1 className={`text-3xl font-semibold mb-3`}>
-            Весь список каталога
-          </h1>
+          <h1 className="text-3xl font-semibold mb-3">Весь список каталога</h1>
         </div>
         <div>
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -209,13 +46,13 @@ export const CatalogContent = () => {
               <li
                 key={id}
                 onClick={() => handleClickProduct(item)}
-                className="overflow-hidden border rounded-sm   border-black w-full cursor-pointer group" // Убрали max-w-[330px] и добавили w-full
+                className="overflow-hidden border rounded-sm border-black w-full cursor-pointer group"
               >
                 <div>
                   <div className="relative w-full h-[250px] overflow-hidden">
                     <img
-                      src={item?.images[0] || '/noImage.png'}
-                      alt={item.location.address || 'Фото квартиры'}
+                      src={item?.images?.[0] || '/noImage.png'}
+                      alt={item?.location?.address || 'Фото квартиры'}
                       className="object-cover w-full h-full md:group-hover:scale-105 duration-1000"
                     />
                     <span className="absolute top-2 right-2 bg-black/50 md:hover:bg-black/70 cursor-pointer duration-300 p-2 rounded-full">
@@ -223,15 +60,11 @@ export const CatalogContent = () => {
                     </span>
                   </div>
 
-                  <div className="p-4 ">
-                    <h2 className="text-md font-bold ">
-                      {['daily', 'weekly', 'monthly', 'yearly'].includes(
-                        item?.price?.paymentPeriod
-                      )
-                        ? `${item?.price?.amount} ${item?.price?.currency === 'USD' ? '$/день' : 'C/день'}`
-                        : 'цена не указана'}
+                  <div className="p-4">
+                    <h2 className="text-md font-bold">
+                      {formatPriceLabel(item.price)}
                     </h2>
-                    <h3 className={`text-sm font-semibold `}>{item?.title}</h3>
+                    <h3 className="text-sm font-semibold">{item?.title}</h3>
 
                     <div className="flex items-center text-gray-600 text-xs">
                       <span>{item.bedrooms}-спалня</span>
@@ -241,13 +74,13 @@ export const CatalogContent = () => {
                       <span>{item.area} м²</span>
                     </div>
                     <div>
-                      <span className={`line-clamp-1 text-xs`}>
+                      <span className="line-clamp-1 text-xs">
                         {item?.location?.address}
                       </span>
                     </div>
                     <div>
-                      <h1 className={`text-xs mt-4`}>Дополнительно:</h1>
-                      <div className="">
+                      <h1 className="text-xs mt-4">Дополнительно:</h1>
+                      <div>
                         <p>{item?.amenities?.join(', ')}</p>
                       </div>
                     </div>
